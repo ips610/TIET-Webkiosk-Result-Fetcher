74a5d1e2ed4523eb6a05c22da906468550026993
Muskan <muskanchalana.26@gmail.com>
2023-06-27 20:13:38 +0530

    modified with while loop

--- TIET Webkiosk Result Fetcher.py
+++ TIET Webkiosk Result Fetcher.py
@@ -1,31 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+
+max_attempts = 5
+current_attempt = 1
+desired_url = "https://webkiosk.thapar.edu/StudentFiles/StudentPage.jsp"
 
 # Set up the Chrome webdriver
 driver = webdriver.Chrome()
 
-# Perform login
-driver.get("https://webkiosk.thapar.edu")
-username = driver.find_element(By.NAME, 'MemberCode')
-password = driver.find_element(By.NAME, 'Password')
-login_button = driver.find_element(By.NAME, 'BTNSubmit')
+while current_attempt <= max_attempts:
+    # Perform login
+    driver.get("https://webkiosk.thapar.edu")
+    username = driver.find_element(By.NAME, 'MemberCode')
+    password = driver.find_element(By.NAME, 'Password')
+    login_button = driver.find_element(By.NAME, 'BTNSubmit')
 
-username.send_keys('test') #enter your own username
-password.send_keys('test') #enter your own password
-login_button.click()
+    username.send_keys(102203274)  # enter your own username
+    password.send_keys(54321)  # enter your own password
+    login_button.click()
 
-print("Success")
+    time.sleep(10)  # Add a small delay to allow the page to load
 
-# Wait for the desired page to load after login
-wait = WebDriverWait(driver, 10)
-wait.until(EC.url_to_be("https://webkiosk.thapar.edu/StudentFiles/StudentPage.jsp"))
+    if driver.current_url == desired_url:
+        print("Success")
 
-while True:
-    try:
-        pass  # You can perform additional actions or tests here
-    except KeyboardInterrupt:
-        break  # Exit the loop on keyboard interrupt (e.g., press Ctrl+C)
+        # to perform further tasks on the opened webpage 
+        while True:
+            try:
+                pass  # You can perform additional actions or tests here
+            except KeyboardInterrupt:
+                break  # Exit the loop on keyboard interrupt (e.g., press Ctrl+C)
+
+        break
+    
+    current_attempt += 1
+    print(f"Attempt {current_attempt} failed. Retrying...")
+
+
+# Clean up
+driver.quit()

4fb62312b4aa74b1d0923ca98a53d3c2285b493a
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-07-24 12:47:18 +0530

    Update emailing.py

--- emailing.py
+++ emailing.py
@@ -3,23 +3,23 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('./.env')
+dotenv_path = Path("./.env")
 load_dotenv(dotenv_path=dotenv_path)
-PASSWORD = os.getenv('PASSWORD')
+PASSWORD = os.getenv("PASSWORD")
 # creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
- 
+s = smtplib.SMTP("smtp.gmail.com", 587)
+
 # start TLS for security
 s.starttls()
- 
+
 # Authentication
 s.login("ishpuneetsingh6@gmail.com", PASSWORD)
- 
+
 # message to be sent
 message = "Hello"
- 
+
 # sending the mail
-s.sendmail("ishpuneetsingh6@gmail.com", "ishpuneetsingh610@gmail.com", message)
- 
+s.sendmail("ishpuneetsingh6@gmail.com", "muskanchalana.26@gmail.com", message)
+print("SENT")
 # terminating the session
 s.quit()
